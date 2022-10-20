bf7040649aa30d9da753b6eeee4d92f0726222cb
Ahmed shahnawaz <shanuthalangara786@gmail.com>
2022-10-20 18:35:27 +0530

    feat(login):added login screen

---
+++ src/components/Login/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import SignUp from '../signup/SignUp';
+import './login.css';
+const Login = () => {
+
+    const [signIn, setSignIn] = useState(false);
+
+  return (
+    <div className='login'>
+        <div className='login__background'>
+            <img className='login__logo' src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt=""/>
+            <button className='login__button' onClick={() => setSignIn(true)}>Sign In</button>
+
+            <div className='login_gradient'/>
+        </div>
+        <div className='login__form'>
+            {signIn ? (
+                <SignUp/>
+            ) : 
+            <>
+            <h1>Unlimited Films, TV programs and more.</h1>
+            <h2>Watch anywhere. Cancel at any time</h2>
+            <h3>Ready to watch? Enter your Email to Create or restart your membership</h3>
+
+            <div className='login__input'>
+                <form>
+                    <input type="email" placeholder="Email Address"/>
+                    <button onClick={() => setSignIn(true)} className='login__getStarted'>GET STARTED</button>
+                </form>
+            </div>
+            </>
+            }
+            
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/signup/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='signUp'></div>
+  )
+}
+
+export default SignUp
